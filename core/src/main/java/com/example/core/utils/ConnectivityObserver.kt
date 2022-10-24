991e3c60c33da8e0c32f0371da8aacc3b61a77fb
Joevincent <jvincent1703@gmail.com>
2022-10-24 19:26:44 +0700

    added access network state permission to manifest

--- core/src/main/java/com/example/core/utils/ConnectivityObserver.kt
+++ core/src/main/java/com/example/core/utils/ConnectivityObserver.kt
@@ -1,12 +1,10 @@
 package com.example.core.utils
 
-import kotlinx.coroutines.flow.Flow
-
-interface ConnectivityObserver {
-
-    fun observe(): Flow<Boolean>
-
-    enum class Status {
-        Available, Unavailable, Losing, Lost
-    }
-}
+//interface ConnectivityObserver {
+//
+//    fun observe(): Flow<Boolean>
+//
+//    enum class Status {
+//        Available, Unavailable, Losing, Lost
+//    }
+//}
--- core/src/main/java/com/example/core/utils/NetworkConnectivityObserver.kt
+++ core/src/main/java/com/example/core/utils/NetworkConnectivityObserver.kt
@@ -1,49 +1,40 @@
 package com.example.core.utils
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
-
-class NetworkConnectivityObserver(
-    private val context: Context
-) : ConnectivityObserver {
-
-    private val connectivityManager =
-        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    override fun observe(): Flow<Boolean> {
-        return callbackFlow {
-            val callback = object : ConnectivityManager.NetworkCallback() {
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    launch { send(true) }
-                }
-
-                override fun onLosing(network: Network, maxMsToLive: Int) {
-                    super.onLosing(network, maxMsToLive)
-                    launch { send(true) }
-                }
-
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                    launch { send(false) }
-                }
-
-                override fun onUnavailable() {
-                    super.onUnavailable()
-                    launch { send(false) }
-                }
-            }
-
-            connectivityManager.registerDefaultNetworkCallback(callback)
-            awaitClose {
-                connectivityManager.unregisterNetworkCallback(callback)
-            }
-        }.distinctUntilChanged()
-    }
-}
+//class NetworkConnectivityObserver(
+//    private val context: Context
+//) : ConnectivityObserver {
+//
+//    private val connectivityManager =
+//        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//
+//    override fun observe(): Flow<Boolean> {
+//        return callbackFlow {
+//            val callback = object : ConnectivityManager.NetworkCallback() {
+//                override fun onAvailable(network: Network) {
+//                    super.onAvailable(network)
+//                    launch { send(true) }
+//                }
+//
+//                override fun onLosing(network: Network, maxMsToLive: Int) {
+//                    super.onLosing(network, maxMsToLive)
+//                    launch { send(true) }
+//                }
+//
+//                override fun onLost(network: Network) {
+//                    super.onLost(network)
+//                    launch { send(false) }
+//                }
+//
+//                override fun onUnavailable() {
+//                    super.onUnavailable()
+//                    launch { send(false) }
+//                }
+//            }
+//
+//            connectivityManager.registerDefaultNetworkCallback(callback)
+//            awaitClose {
+//                connectivityManager.unregisterNetworkCallback(callback)
+//            }
+//        }.distinctUntilChanged()
+//    }
+//}
